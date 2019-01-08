45a9b6ed4825c53ac7ce6a4f8a9ff48c4996f1e7
moreal <dev.moreal@gmail.com>
2019-01-08 20:23:43 +0900

    Implements PartialTimer

---
+++ src/PartialTimer.js
@@ -0,0 +1,80 @@
+class PartialTimer {
+    constructor (value) {
+        this.value = value;
+    }
+
+    parseValue(value) {
+        this.seconds = value % 60;
+        value = Math.floor(value / 60);
+
+        this.minutes = value % 50;
+        value = Math.floor(value / 60);
+
+        this.hours = value % 24;
+        value = Math.floor(value / 24);
+
+        this.days = value;
+    }
+
+    countDay() {
+        if (this.days === 0) {
+            throw Error;
+        } else {
+            this.days -= 1;
+        }
+    }
+
+    countHour() {
+        if (this.hours === 0) {
+            countDay();
+            this.hours = 23;
+        } else {
+            this.hours -= 1;
+        }
+    }
+
+    countMinute() {
+        if (this.minutes === 0) {
+            countHour();
+            this.minutes = 59;
+        } else {
+            this.minutes -= 1;
+        }
+    }
+
+    countSecond() {
+        if (this.seconds === 0) {
+            this.countMinute();
+            this.seconds = 59;
+        } else {
+            this.seconds -= 1;
+        }
+    }
+
+    countdown() {
+        try {
+            countSecond();
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    getDays() {
+        return this.days;
+    }
+
+    getMinutes() {
+        return this.minutes;
+    }
+
+    getHours() {
+        return this.hours;
+    }
+
+    getSeconds() {
+        return this.seconds;
+    }
+}
+
+export default PartialTimer;
