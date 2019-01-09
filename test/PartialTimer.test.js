1539fbc4e96cfef304cfa2e37cc0b7e7a560b859
moreal <dev.moreal@gmail.com>
2019-01-09 15:25:59 +0900

    Complete to write PartialTimer test code

--- test/PartialTimer.test.js
+++ test/PartialTimer.test.js
@@ -1,8 +1,47 @@
-let assert = require('assert');
-let PartialTimer = require('../src/PartialTimer');
+import PartialTimer from '../src/PartialTimer';
+import assert from 'assert';
 
-describe("Test", ()=> {
-    it('it', ()=>{
-        
-    })
-})
+describe('PartialTimer 유닛테스트', function() {
+    describe('정상적으로 파싱 가능한지', function() {
+        it('Getter 테스트 - Clean case', function() {
+            let timer = new PartialTimer(3600);
+            assert.equal(0, timer.getDays());
+            assert.equal(1, timer.getHours());
+            assert.equal(0, timer.getMinutes());
+            assert.equal(0, timer.getSeconds());
+        });
+
+        it('Getter 테스트 - Dirty case', function() {
+            let timer = new PartialTimer(172799);
+            assert.equal(1, timer.getDays());
+            assert.equal(23, timer.getHours());
+            assert.equal(59, timer.getMinutes());
+            assert.equal(59, timer.getSeconds());
+        });
+    });
+
+    describe('정상적인 카운트 다운이 되는지', function() {
+        it('countdown 테스트 - Case D-1 23:59:59', function() {
+            let timer = new PartialTimer(172799);
+            timer.countdown();
+            assert.equal(1, timer.getDays());
+            assert.equal(23, timer.getHours());
+            assert.equal(59, timer.getMinutes());
+            assert.equal(58, timer.getSeconds());
+        });
+
+        it('countdown 테스트 - Case D-2 00:00:00', function() {
+            let timer = new PartialTimer(172800);
+            timer.countdown();
+            assert.equal(1, timer.getDays());
+            assert.equal(23, timer.getHours());
+            assert.equal(59, timer.getMinutes());
+            assert.equal(59, timer.getSeconds());
+        });
+
+        it('countdown 테스트 - Case D-0 00:00:00', function() {
+            let timer = new PartialTimer(0);
+            assert.notEqual(true, timer.countdown());
+        });
+    });
+});
